f06951414effcb15508fbff9ef504b9b08b7f4bf
dubuqingfeng <1135326346@qq.com>
2019-12-24 20:09:17 +0800

    run.sh.

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ scrapy runspider spiders/poolin.py
 scrapy runspider spiders/huobipool.py
 scrapy runspider spiders/antpool.py
 scrapy runspider spiders/sparkpool.py
+scrapy runspider spiders/viabtc.py
+scrapy runspider spiders/f2pool.py
